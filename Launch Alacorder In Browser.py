d4e572434f0aff2c4864a16d11df42db2a35231b
sbrobson959 <123346021+sbrobson959@users.noreply.github.com>
2023-02-05 06:21:19 +0000

    addlink

---
+++ Launch Alacorder In Browser.py
@@ -0,0 +1,15 @@
+'''
+ALACORDER beta 6.3.2
+
+Click Run in top right corner to start ALACORDER.
+
+To install locally, open your preferrred Python environment and run "pip install alacorder" or "pip3 install alacorder" to install, 
+then "python -m alacorder" or "python3 -m alacorder". When new versions are released, it is recommended that you uninstall your
+copy of alacorder with "pip uninstall alacorder" or "pip3 uninstall alacorder", then reinstall with "pip install alacorder" or "pip3 install alacorder".
+
+If you don't know Python, download Anaconda Distribution, set up a virtual environment, open a terminal, and then use the instructions above to load alacorder.
+(https://www.anaconda.com/products/distribution)
+
+
+'''
+from alacorder import __main__
