967452d82a2ff7ed331a9ce630dec11c63ba090c
Sam <sbrobson@crimson.ua.edu>
2023-03-13 17:18:56 -0400

    push

--- src/alacorder/__main__.py
+++ src/alacorder/__main__.py
@@ -34,7 +34,7 @@ pd.set_option('display.max_rows', 100)
 ## COMMAND LINE INTERFACE
 
 @click.group()
-@click.version_option("77.5", package_name="alacorder")
+@click.version_option("77.5.1", package_name="alacorder")
 def cli():
     """
     ALACORDER beta 77.5
