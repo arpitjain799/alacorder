0bbbef13fdf3578e5b69accb6071055880776db8
Sam <sbrobson@crimson.ua.edu>
2023-03-18 16:40:11 -0500

    push

--- src/alacorder/alac.py
+++ src/alacorder/alac.py
@@ -43,6 +43,7 @@ def append_archive(in_path, out_path, no_write=False, obj=False):
          input_archive = read(in_path)
          output_archive = read(out_path)
          new_archive = pd.concat([output_archive, input_archive], ignore_index=True)
+         new_archive = new_archive.dropna()
          if not no_write:
             cin = setinputs(input_archive)
             cout = setoutputs(out_path)
