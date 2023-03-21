be0bfbfd1187545e310430f4c14a51bf8d666c05
Sam <sbrobson@crimson.ua.edu>
2023-03-21 03:04:41 -0500

    push

--- alacorderlite/src/alacorderlite/alacorder.py
+++ alacorderlite/src/alacorderlite/alacorder.py
@@ -2,7 +2,6 @@
 # sam robson
 
 import PySimpleGUI as sg
-
 import glob
 import inspect
 import math
@@ -35,7 +34,7 @@ pd.set_option('display.max_categories', 16)
 pd.set_option('display.precision',2)
 
 
-tqdm.pandas(ncols=80,bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} cases')
+tqdm.pandas(ncols=80,bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}')
 warnings.filterwarnings('ignore')
 
 uni_status = ""
@@ -46,23 +45,32 @@ uni_total = 0
 version = "ALACORDER beta 78.3 (lite)"
 
 sg.theme("DarkBlack")
-sg.set_options(font="Default 13")
+sg.set_options(font="Default 14")
 
 current_label = f"{version}"
 fetch_layout = [
     [sg.Text("""Collect case PDFs in bulk from Alacourt.com\nfrom a list of names or search parameters.""",font="Default 22",pad=(10,10))],
-    [sg.Text("""Use column headers NAME, PARTY_TYPE, SSN, DOB, COUNTY, DIVISION, CASE_YEAR, and/or FILED_BEFORE in an Excel spreadsheet to submit a list of queries for Alacorder to scrape. Each column corresponds to a search field in Party Search. Missing columns and entries will be left empty, i.e. if only the NAME's and CASE_YEAR's are relevant to the search, a file with two columns will work.""",size=(55,None))],
+    [sg.Text("""Use column headers NAME, PARTY_TYPE, SSN, DOB, COUNTY, DIVISION,
+CASE_YEAR, and/or FILED_BEFORE in an Excel spreadsheet to submit a list of
+queries for Alacorder to scrape. Each column corresponds to a search field
+in Party Search. Missing columns and entries will be left empty, i.e. if only 
+the NAME's and CASE_YEAR's are relevant to the search, a file with two
+columns will work.""", pad=(5,5))],
     [sg.Text("Input Path: "), sg.InputText(key="SQ-INPUTPATH-",focus=True)],
     [sg.Text("Output Path: "), sg.InputText(key="SQ-OUTPUTPATH-")],
     [sg.Text("Alacourt.com Credentials", font="Default 14")],
-    [sg.Text("Customer ID: "), sg.Input(key="SQ-CUSTOMERID-")],
-    [sg.Text("User ID: "), sg.Input(key="SQ-USERID-")],
-    [sg.Text("Password: "), sg.InputText(key="SQ-PASSWORD-",password_char='*')],
+    [sg.Text("Customer ID: "), sg.Input(key="SQ-CUSTOMERID-",size=(20,1))],
+    [sg.Text("User ID: "), sg.Input(key="SQ-USERID-",size=(20,1))],
+    [sg.Text("Password: "), sg.InputText(key="SQ-PASSWORD-",password_char='*',size=(20,1))],
     [sg.Text("Max queries: "), sg.Input(key="SQ-MAX-", default_text="0", size=[5,1]),sg.Text("Skip from top: "), sg.Input(key="SQ-SKIP-", default_text="0",size=[5,1]),sg.Text("Speed Multiplier: "), sg.Input(key="SQ-SPEED-", default_text="1",size=[5,1])],
     [sg.Button("Start Query",key="SQ")]] # "SQ"
 archive_layout = [
-    [sg.Text("""Create full text archives from a\ndirectory with PDF cases.""", font="Default 22", pad=(10,10), size=(60,None))],
-    [sg.Text("""Case text archives require a fraction of the storage capacity and processing time used to process PDF directories. Before exporting your data to tables, create an archive with supported file extensions .pkl.xz, .json(.zip), and .csv(.zip). Once archived, use your case text archive as an input for multitable or single table export.""",size=(50,None), font='Default 14')],
+    [sg.Text("""Create full text archives from a\ndirectory with PDF cases.""", font="Default 22", pad=(10,10))],
+    [sg.Text("""
+Case text archives require a fraction of the storage capacity and processing time used
+to process PDF directories. Before exporting your data to tables, create an archive
+with supported file extensions .pkl.xz, .json(.zip), and .csv(.zip). Once archived, use your
+case text archive as an input for multitable or single table export.""", pad=(5,5))],
     [sg.Text("Input Path: "), sg.InputText(key="MA-INPUTPATH-")],
     [sg.Text("Output Path: "), sg.InputText(key="MA-OUTPUTPATH-")],
     [sg.Text("Skip Cases in Archive: "), sg.Input(key="MA-SKIP-")],
@@ -70,48 +78,69 @@ archive_layout = [
     [sg.Checkbox("Try to Append",key="MA-APPEND-", default=True), sg.Checkbox("Allow Overwrite",default=True,key="MA-OVERWRITE-")],
     [sg.Button("Make Archive",key="MA")]] # "MA"
 table_layout = [
-    [sg.Text("""Export data tables from case archive or directory.""", font="Default 22", size=(50,1), pad=(10,10))],
-    [sg.Text("""Alacorder processes case detail PDFs and case text archives into data tables suitable for research purposes. Export an Excel spreadsheet with detailed cases information (cases), fee sheets (fees), and charges information (charges, disposition, filing), or select a table to export to another format (.json, .csv). Note: It is recommended that you create a case text archive from your target PDF directory before exporting tables. Case text archives can be processed into tables at a much faster rate and require far less storage.""", size=(50,None))],
+    [sg.Text("""Export data tables from\ncase archive or directory.""", font="Default 22", pad=(10,10))],
+    [sg.Text("""Alacorder processes case detail PDFs and case text archives into
+data tables suitable for research purposes. Export an Excel spreadsheet
+with detailed cases information(cases), fee sheets (fees), and charges
+information (charges, disposition, filing), or select a table to export to
+another format (.json, .csv). Note: It is recommended that you create a
+case text archive from your target PDF directory before exporting tables.
+Case text archives can be processed into tables at a much faster rate and
+require far less storage.""", pad=(5,5))],
     [sg.Text("Input Path: "), sg.InputText(key="TB-INPUTPATH-")],
     [sg.Text("Output Path: "), sg.InputText(key="TB-OUTPUTPATH-")],
     [sg.Text("Max cases: "), sg.Input(key="TB-COUNT-", default_text="0", size=[10,1])],
     [sg.Radio("All Tables (.xlsx, .xls)", "TABLE", key="TB-ALL-", default=True), 
         sg.Radio("Cases", "TABLE", key="TB-CASES-", default=False), 
         sg.Radio("All Charges", "TABLE", key="TB-CHARGES-", default=False)], 
-    [sg.Radio("Disposition Charges", "TABLE", key="TB-DISPOSITION-",default=False), sg.Radio("Filing Charges", "TABLE", key="TB-FILING-",default=False),
-        sg.Radio("Fee Sheets","TABLE",key="TB-FEES-",default=False)],
+    [sg.Radio("Disposition Charges", "TABLE", key="TB-DISPOSITION-",default=False), sg.Radio("Filing Charges", "TABLE", key="TB-FILING-",default=False), sg.Radio("Fee Sheets","TABLE",key="TB-FEES-",default=False)],
     [sg.Checkbox("Allow Overwrite", key="TB-OVERWRITE-"), sg.Checkbox("Compress", key="TB-COMPRESS-")],
     [sg.Button("Export Table",key="TB")]] # "TB"
 append_layout = [
-    [sg.Text("""Append case text archive with the contents\nof a PDF directory or the contents of another archive.""", font="Default 22", size=(60,None), pad=(10,10))],
-    [sg.Text("""Case text archives require a fraction of the storage capacity and processing time used to process PDF directories. Before exporting your data to tables, create an archive with supported file extensions .pkl.xz, .json(.zip), and .csv(.zip). Once archived, use your case text archive as an input for multitable or single table export.""",auto_size_text=True, font='Default 14', size=(50,None))],
+    [sg.Text("""Append case text archive with the contents\nof a case directory or archive.""", font="Default 22", pad=(10,10))],
+    [sg.Text("""Case text archives require a fraction of the storage capacity
+and processing time used to process PDF directories. Before exporting your
+data to tables, create an archive with supported file extensions .pkl.xz,
+.json(.zip), and .csv(.zip). Once archived, use your case text archive as
+an input for multitable or single table export.""", pad=(5,5))],
     [sg.Text("Input Path: "), sg.InputText(key="AA-INPUTPATH-")],
     [sg.Text("Output Path: "), sg.InputText(key="AA-OUTPUTPATH-")],
     [sg.Button("Append Archives", key="AA")]] # "AA"
 mark_layout = [
-    [sg.Text("""Mark query template with collected cases from input\narchive or directory.""", font="Default 22", size=(60,None), pad=(10,10))],
-    [sg.Text("""Use column headers NAME, PARTY_TYPE, SSN, DOB, COUNTY, DIVISION, CASE_YEAR, and/or FILED_BEFORE in an Excel spreadsheet to submit a list of queries for Alacorder to scrape. Each column corresponds to a search field in Party Search. Missing columns and entries will be left empty, i.e. if only the NAME's and CASE_YEAR's are relevant to the search, a file with two columns will work.""", font='Default 14', size=(60,None))],
+    [sg.Text("""Mark query template with collected cases\nfrom input archive or directory.""", font="Default 22", pad=(10,10))],
+    [sg.Text("""Use column headers NAME, PARTY_TYPE, SSN, DOB, COUNTY, DIVISION,
+CASE_YEAR, and/or FILED_BEFORE in an Excel spreadsheet to submit a list of
+queries for Alacorder to scrape. Each column corresponds to a search field
+in Party Search. Missing columns and entries will be left empty, i.e. if
+only the NAME's and CASE_YEAR's are relevant to the search, a file with two
+columns will work.""", pad=(5,5))],
     [sg.Text("Input Path: "), sg.InputText(key="MQ-INPUTPATH-")],
     [sg.Text("Output Path: "), sg.InputText(key="MQ-OUTPUTPATH-")],
     [sg.Button("Mark Query",key="MQ")]] # "MQ"
 about_layout = [
     [sg.Text(f"""\n{version}""",font="Courier 22", pad=(5,5))],[sg.Text("""Alacorder retrieves and processes case detail PDFs\ninto data tables suitable for research purposes.""",font="Default 22", pad=(10,10))],
-    [sg.Text("""1.  fetch - Retrieve case detail PDFs in bulk from Alacourt.com\n2.  archive - Create full text archives from PDF directory\n3.  table - Export data tables from case archive or directory\n4.  append - Append contents of one archive to another\n5.  mark - Mark already collected cases on query template
-        """, font="Courier 14", size=[65,None])],
-    [sg.Text("View documentation, source code, and latest updates on GitHub (https://github.com/sbrobson959/alacorder) or PyPI (https://pypi.org/project/alacorder/). To use command line interface, run `% python -m alacorder --help` for guidance. \n\n© 2023 Sam Robson", font="Default 14", size=[60,None])],
+    [sg.Text(
+        """1.  fetch - Retrieve case detail PDFs in bulk from Alacourt.com
+2.  archive - Create full text archives from PDF directory
+3.  table - Export data tables from case archive
+or directory
+4.  append - Append contents of one archive to another
+5.  mark - Mark already collected cases on query template""", font="Courier 14")],
+    [sg.Text("""View documentation, source code, and latest updates at
+github.com/sbrobson959/alacorder or pypi.org/project/alacorder/.\n\n© 2023 Sam Robson""")],
     ] # "ABOUT"
-tabs = [sg.TabGroup(size=[575,None], expand_x=True, expand_y=True,font="Courier",layout=[
-                    [sg.Tab("fetch", layout=fetch_layout, pad=(10,10))],
-                    [sg.Tab("archive", layout=archive_layout, pad=(10,10))],            
-                    [sg.Tab("table", layout=table_layout, pad=(10,10))],
-                    [sg.Tab("append", layout=append_layout, pad=(10,10))],
-                    [sg.Tab("mark", layout=mark_layout, pad=(10,10))],
-                    [sg.Tab("about", layout=about_layout, pad=(10,10))]])]
+tabs = sg.TabGroup(expand_x=True, expand_y=True,font="Courier",layout=[
+                    [sg.Tab("fetch", layout=fetch_layout, pad=(2,2))],
+                    [sg.Tab("archive", layout=archive_layout, pad=(2,2))],            
+                    [sg.Tab("table", layout=table_layout, pad=(2,2))],
+                    [sg.Tab("append", layout=append_layout, pad=(2,2))],
+                    [sg.Tab("mark", layout=mark_layout, pad=(2,2))],
+                    [sg.Tab("about", layout=about_layout, pad=(2,2))]])
 
 
-layout = [[tabs]]
+layout = [[tabs],[sg.Sizegrip()]]
 
-window = sg.Window(title="ALACORDER (graphical beta)", layout=layout, finalize=True)
+window = sg.Window(title="Alacorder 78", layout=layout, finalize=True, grab_anywhere=True, resizable=True) # FYI: bool: enable_window_config_events -  If True then window configuration events (resizing or moving the window) will return WINDOW_CONFIG_EVENT from window.read. Note you will get several when Window is created.
 
 ## ALAC
 
@@ -2319,8 +2348,6 @@ def party_search(driver, name = "", party_type = "", ssn="", dob="", county="", 
    try:
       results_indicator = driver.find_element(by=By.ID, value="ContentPlaceHolder1_lblResultCount")
       results_count = int(results_indicator.text.replace("Search Results: ","").replace(" records returned.","").strip())
-      if results_count == 1000 and debug or no_log:
-         print(f"Max records (1000) returned for party {name}!")
    except:
       pass
 
@@ -2661,6 +2688,7 @@ while awaiting_input:
             awaiting_input = False
             window.close()
             init(cf)
+            sg.popup_ok("Alacorder completed the task.")
             break
         except:
             continue
@@ -2676,6 +2704,7 @@ while awaiting_input:
             awaiting_input = False
             archive(aa)
             window.close()
+            sg.popup_ok("Alacorder completed the task.")
             break
         except:
             continue
@@ -2694,6 +2723,7 @@ while awaiting_input:
             awaiting_input = False
             window.close()
             fetch(window['SQ-INPUTPATH-'].get(),window['SQ-OUTPUTPATH-'].get(),cID=window['SQ-CUSTOMERID-'].get(),uID=window['SQ-USERID-'].get(),pwd=pwd,qmax=sq_max,speed=sq_speed,qskip=sq_skip,no_log=True)
+            sg.popup_ok("Alacorder completed the task.")
             break
         except:
             continue
@@ -2703,6 +2733,7 @@ while awaiting_input:
             awaiting_input = True
             window.close()
             mark(window['MQ-INPUTPATH-'].get(),window['MQ-OUTPUTPATH-'].get())
+            sg.popup_ok("Alacorder completed the task.")
             break
         except:
             continue
@@ -2712,19 +2743,10 @@ while awaiting_input:
             take_inputs = False
             window.close()
             append_archive(window['AA-INPUTPATH-'].get(),window['AA-OUTPUTPATH-'].get())
+            sg.popup_ok("Alacorder completed the task.")
             break
         except:
             print("Error: check configuration.")
-    elif event == "TB-COMPLETE-":
-        sg.popup_ok("Alacorder completed table export.")
-    elif event == "MA-COMPLETE-":
-        sg.popup_ok("Alacorder completed text archive.")
-    elif event == "SQ-COMPLETE-":
-        sg.popup_ok("Alacorder completed search query.")
-    elif event == "MQ-COMPLETE-":
-        sg.popup_ok("Alacorder marked collected cases in search query.")
-    elif event == "AA-COMPLETE-":
-        sg.popup_ok("Alacorder appended archives successfully.")
 
 window.close()
 
